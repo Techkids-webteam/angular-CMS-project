493a2beac078bee4fe28af80c7d2ee9f7a2beb97
hiepxanh <hiepxanh@gmail.com>
2016-07-06 10:42:24 +0700

    deploy change port 6969

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'port': process.env.PORT || 8080,
+  'port': process.env.PORT || 6969,
   'database':'mongodb://techkids:codethechange@ds021751.mlab.com:21751/techkids',
   'secret':'ilovescotchscotchyscotchscotch'
 };
