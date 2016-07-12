f3ff1dcbe76d42e3987eb21e9dc569f8f39fcf23
hiepxanh <hiepxanh@gmail.com>
2016-07-12 11:30:09 +0700

    hiep doi cong 3103
    doi cong 3103

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'port': process.env.PORT || 6969,
+  'port': process.env.PORT || 3103,
   'database':'mongodb://techkids:codethechange@ds021751.mlab.com:21751/techkids',
   'secret':'ilovescotchscotchyscotchscotch'
 };
